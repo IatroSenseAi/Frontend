c9857f79090245e587e0f0dee48fe0705afd25a0
Abderrahmen Talbi <129063494+AbderrahmenTalbi@users.noreply.github.com>
2025-09-06 18:46:25 +0100

    Deployement issues

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const getBaseURL = () => {
-  if (import.meta.env.NODE_ENV === "development") {
-    return import.meta.env.API_BASE_URL;
+  if (import.meta.env.MODE === "development") {
+    return import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
   }
-  return import.meta.env.API_BASE_URL_PROD;
+  return import.meta.env.VITE_API_BASE_URL_PROD || "https://iatrosenseai.onrender.com/api";
 };
 
 export const axiosInstance = axios.create({
